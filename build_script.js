4b1630760354c5131d81179e9d518b8de917dde8
JohnPaulInso <88543202+JohnPaulInso@users.noreply.github.com>
2026-02-28 00:41:44 +0800

    Add files via upload

--- build_script.js
+++ build_script.js
@@ -1,5 +1,18 @@
 const fs = require('fs');
-const members = JSON.parse(fs.readFileSync('infos.json', 'utf8'));
+let members = JSON.parse(fs.readFileSync('infos.json', 'utf8'));
+
+// Clean data: remove null/undefined and assign deterministic IDs
+members = members.map((m, index) => {
+    const obj = { ...m };
+    if (!obj.id) obj.id = `ASC-${(index + 1).toString().padStart(3, '0')}`;
+    Object.keys(obj).forEach(key => {
+        if (obj[key] === undefined || obj[key] === null) {
+            delete obj[key];
+        }
+    });
+    return obj;
+});
+
 const script = `(async () => {
     if (!window.FirebaseService || !window.FirebaseService.currentUser) {
         alert('Please log in to the main ASC Tracker app first!');
